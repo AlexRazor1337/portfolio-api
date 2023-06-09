acadef43fb3b457444db9dab94aa5d48910d9c99
AlexRazor <alexrazor1337@gmail.com>
2023-06-09 11:23:46 +0300

    Auth routes boilerplate

---
+++ app/routes/auth/auth.router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { StatusCodes } = require('http-status-codes')
+
+const { validator } = require('@/middleware');
+
+const validation = require('./auth.validation');
+const authController = require('./auth.controller');
+
+const authRouter = express.Router();
+
+authRouter.post('/signup', validator(validation.signup), (req, res, next) =>
+    authController
+        .signup(req.body)
+        .then((result) => res.status(StatusCodes.OK).send(result))
+        .catch(next)
+);
+
+module.exports = authRouter;
