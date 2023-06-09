89563279670f6ac134ce65f9da0b1da7d1b12d9e
AlexRazor <alexrazor1337@gmail.com>
2023-06-09 11:02:11 +0300

    Add middleware validator

---
+++ app/routes/middleware/validator.js
@@ -0,0 +1,14 @@
+const validator = (schema) => (req, res, next) => {
+    const { body, params, query } = req;
+
+    const request = { body, params, query };
+    // Remove body, params, quert from request if they are empty objects for validation
+    Object.keys(request).forEach((key) => (Object.keys(request[key]).length === 0) && delete request[key]);
+
+    const { error } = schema.validate(request);
+    if (error) throw new Error(error.details[0].message)
+
+    next();
+}
+
+module.exports = validator;
