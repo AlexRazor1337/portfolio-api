1c12f1da1f4c83bf1bbef8dd0eb1e55fe4bbbce8
AlexRazor <alexrazor1337@gmail.com>
2023-06-09 14:32:21 +0300

    Auth middleware

---
+++ app/middleware/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const { StatusCodes } = require('http-status-codes');
+const InvalidatedToken = require('@/models/invalidated-token');
+
+const auth = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+        throw new Error('Missing authorization token');
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        InvalidatedToken.findOne({
+            where: {
+                token,
+            },
+        }).then((result) => {
+            if (result) {
+                res.status(StatusCodes.UNAUTHORIZED).json({
+                    error: 'Invalid token',
+                });
+            }
+
+            req.user = decoded;
+            req.token = token;
+            next();
+        });
+    } catch (err) {
+        res.status(StatusCodes.UNAUTHORIZED).json({
+            error: 'Invalid token',
+        });
+    }
+}
+
+module.exports = auth;
